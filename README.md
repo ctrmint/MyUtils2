7de8eee871ebb678feea4adb39d25c3f961ba871
mark <mark@mrodman.com>
2021-05-31 18:10:58 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # MyUtils
 Contains sample code for handy stuff which might be needed in the future
+
--- main.py
+++ main.py
@@ -1,23 +1,24 @@
 # A set of classes and utils for reuse in other projects.
 # Being collate here for reference, copy and paste to other projects.
 
-class EmailAddres(object):
+class EmailAddress(object):
     """
     A class for processing email addresses
     """
     def __init__(self, emailAddr):
-        self.emailAddr = str(emailAddr)
 
+        self.emailAddr = str(emailAddr)
         self.emailAddrName = self.emailAddr.split("@")[0]
         self.emailAddrDomain = self.emailAddr.split("@")[1]
+
 
     def __str__(self):
         return self.emailAddr + "," +self.emailAddrName
 
 
 def main():
-    emailaddr = EmailAddres("peter.rabbit@microsoft.com")
-    print(emailaddr.__dict__)
+    emailaddr = EmailAddress("peter.rabbit@microsoft.com")
+
     return
 
 
