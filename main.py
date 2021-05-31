b24f99a8271ff0cc20ec47c6117148b8fb33c658
mark <mark@mrodman.com>
2021-05-31 18:28:14 +0100

    initial commit

--- main.py
+++ main.py
@@ -1,27 +1,44 @@
 # A set of classes and utils for reuse in other projects.
 # Being collate here for reference, copy and paste to other projects.
+import re
+
 
 class EmailAddress(object):
     """
     A class for processing email addresses
     """
-    def __init__(self, emailAddr):
 
-        self.emailAddr = str(emailAddr)
-        self.emailAddrName = self.emailAddr.split("@")[0]
-        self.emailAddrDomain = self.emailAddr.split("@")[1]
+    def __init__(self, email_addr):
+        self.emailAddr = str(email_addr)
+        self.emailAddr_valid = True
 
+        if self.validate_emailaddr(self.emailAddr):
+            self.emailAddrName = self.emailAddr.split("@")[0]
+            self.emailAddrDomain = self.emailAddr.split("@")[1]
+        else:
+            self.emailAddr_valid = False
+
+    @staticmethod
+    def validate_emailaddr(emailaddr):
+        """
+        Check's the email address supplied is valid.
+        :param emailaddr:
+        :return:
+        """
+        regex = '^(\w|\.|\_|\-)+[@](\w|\_|\-|\.)+[.]\w{2,3}$'
+        if re.search(regex, emailaddr):
+            return True
+        return False
 
     def __str__(self):
-        return self.emailAddr + "," +self.emailAddrName
+        return self.emailAddr + "," + self.emailAddrName
 
 
 def main():
     emailaddr = EmailAddress("peter.rabbit@microsoft.com")
-
+    print(emailaddr.__dict__)
     return
 
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     main()
